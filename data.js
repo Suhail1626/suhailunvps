a57173980a2d268ca331b639a2daacdadeca3950
Suhail1626 <104824273+Suhail1626@users.noreply.github.com>
2022-07-16 08:28:28 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://Suhail:suhail123@cluster0.jjayw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+    
+    bot_token:'5343317733:AAHGWZlc__z1uLewkFKfbMU1ErT35yyZJ-c',//bot token
+    
+    bot_name:'DmounVpsAirdropBot',//bot username without @
+    
+    bot_admin: 1714908211,// admin telegram id
+    
+    channelsList : ['@AigoAirdropsOfficial'],// channels list
+    
+    reffer_bonus:500000,// refferal bonus amount
+    
+    min_wd:1000000,//min withdrawal amount
+    
+    daily_bonus:200000,// daily bonus amount
+    
+    currency:'CIX', //bot cuurency
+    
+    cb_api_key:'',//coinbase api key
+    
+    cb_api_secret:'',// coinbase secret key
+    
+    cb_account_id:'',// counbase account id for the bot currency
+    
+    payment_channel:'@RealBotPayouts'// payment channel username
+    
+    }
